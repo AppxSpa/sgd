61d25d4581c96f6e9fcbb04e0d6cff5979da93ae
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-21 18:22:01 -0300

    agrega dockerfiles

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+sudo docker stop sgd-container 2>/dev/null
+sudo docker rm sgd-container 2>/dev/null
+
+sudo docker build -t sgd .
+
+sudo docker run \
+           --restart always \
+           -d -p 8090:8090 \
+           --env-file .env \
+           --network appx \
+           --add-host=host.docker.internal:host-gateway \
+           --name sgd-container sgd
